9d300d551b627a598e3e771bce51caac237edd41
Subodh <subodhkulkarni48@gmail.com>
2020-11-03 21:33:18 +0530

    Fixed error

--- app1.py
+++ app1.py
@@ -12,6 +12,7 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'subodh'
+db.init_app(app)
 api = Api(app)
 
 @app.before_first_request
@@ -28,5 +29,5 @@ api.add_resource(StoreList,'/stores')
 api.add_resource(UserRegister,'/register')
 
 if __name__ == '__main__':
-    db.init_app(app)
+    
     app.run(port=5000,debug =True)
